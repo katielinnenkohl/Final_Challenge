9ba9f60af79d75e80a67ac6a93bd07327e491651
Katherine R Linnenkohl <kl88081@srcs.loc>
2017-12-15 11:33:49 -0800

    times table

---
+++ Final_Challenge.js
@@ -0,0 +1,23 @@
+function checkPrime(number){
+    for ( i = 2; i <= (number) / 2; i++){
+        if (number % i == 0){
+            return "not prime";
+        }
+    }
+    return "prime";
+
+}
+function timesTable(number){
+var rows = number +1;
+var cells = number +1;
+document.write ("<table border = 1>");
+for (i = 1; i < rows; i++){
+    document.write ("<tr>");
+
+for (j = 1; j < cells; j++){
+    document.write ("<td>" , i*j , "</td>");
+}
+    document.write("</tr>");
+}
+    document.write("</table")
+    }
