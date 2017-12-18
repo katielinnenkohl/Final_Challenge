7b19abb69df2741f66852acee6ea4c892808e6e1
katielinnenkohl <katielinnenkohl@gmail.com>
2017-12-17 17:09:36 -0800

    fixed

--- Final_Challenge.js
+++ Final_Challenge.js
@@ -1,4 +1,7 @@
-function checkPrime(number){
+function checkPrime(number) {
+    if ((number) <= 1 || (number) != Math.floor(number)) {
+        return "not prime"
+    }
     for ( i = 2; i <= (number) / 2; i++){
         if (number % i == 0){
             return "not prime";
@@ -8,6 +11,21 @@ function checkPrime(number){
 
 }
 function timesTable(number){
+var rows = number +1;
+var cells = number +1;
+document.write ("<table border = 1>");
+for (i = 1; i < rows; i++){
+    document.write ("<tr>");
+
+for (j = 1; j < cells; j++){
+    document.write ("<td>" , i*j , "</td>");
+}
+    document.write("</tr>");
+}
+    document.write("</table")
+    }
+
+function primeTable(number){
 var rows = number +1;
 var cells = number +1;
 document.write ("<table border = 1>");
